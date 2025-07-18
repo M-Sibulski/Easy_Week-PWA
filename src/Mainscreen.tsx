bda26cd90b9c402aca7f95e521922c59818e44fa
Mauricio Sibulski <86140316+M-Sibulski@users.noreply.github.com>
2025-07-18 17:18:54 +1200

    Revert "Enhance transactions UI"

--- src/Mainscreen.tsx
+++ src/Mainscreen.tsx
@@ -3,18 +3,16 @@ import { db } from '../db.ts';
 import { Transactions } from '../db.ts';
 import './App.css';
 import Day from './Day.tsx';
-import { dateToInputType } from './dateConversions.ts';
 
 const Mainscreen = () => {
     const transactions = useLiveQuery<Transactions[]>(() => db.transactions.where("name").notEqual('').sortBy('date'));
-    // const dates = Array.from(new Set(transactions?.map(t => t.date)))
-    const dateNames = Array.from(new Set(transactions?.map(t => dateToInputType(t.date))))
+    const dates = Array.from(new Set(transactions?.map(t => t.date)))
     console.log('Mainscreen');
 
 
   return (
-    <div className='h-full overflow-y-auto flex flex-col min-h-full gap-2 p-1 bg-gray-300'>
-        {transactions && dateNames.map(d => <Day key={d} date={d} transactions={transactions.filter((t2 => dateToInputType(t2.date) == d))} total={transactions.filter(t => dateToInputType(t.date) <= d).reduce((accumulator, transaction) => accumulator + transaction.value, 0)}/>)}
+    <div className='h-full overflow-y-auto flex flex-col min-h-full gap-2 p-1 rounded-xl shadow-md bg-white'>
+        {transactions && dates.map(d => <Day key={d} date={d} transactions={transactions.filter((t2 => t2.date == d))} total={transactions.filter(t => t.date <= d).reduce((accumulator, transaction) => accumulator + transaction.value, 0)}/>)}
     </div>
   )
 }
