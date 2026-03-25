3670537ce3c67f67c05fbaac871e0a73491be138
Mauricio <mauricio.sibulski@outlook.com>
2026-03-25 20:26:31 +1300

    Updated dependencies

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,10 @@
 import 'fake-indexeddb/auto';
+import { vi } from 'vitest';
+
+vi.mock('virtual:pwa-register/react', () => ({
+	useRegisterSW: () => ({
+		offlineReady: [false, vi.fn()],
+		needRefresh: [false, vi.fn()],
+		updateServiceWorker: vi.fn(),
+	}),
+}));
